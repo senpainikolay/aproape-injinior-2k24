ff9bb4fced4e7a1e5bdc62bc7abba1527fff180a
senpainikolay <nicolae32gherman@gmail.com>
2024-04-19 20:25:54 +0300

    add basic auth service

---
+++ backend/authentication-service/main.py
@@ -0,0 +1,70 @@
+from flask import Flask, jsonify
+from extensions import db, jwt
+from dotenv import load_dotenv
+from models import User,TokenBlocklist
+from auth import auth_bp
+
+load_dotenv()
+
+
+
+def create_app():
+
+    app = Flask(__name__)
+
+    app.config.from_prefixed_env()
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+
+    # load user
+    @jwt.user_lookup_loader
+    def user_lookup_callback(_jwt_headers, jwt_data):
+        identity = jwt_data["sub"]
+        return User.query.filter_by(id=identity).one_or_none()
+    
+    # jwt error handlers
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_data):
+        return jsonify({"message": "Token has expired", "error": "token_expired"}), 401
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "message": "Request doesnt contain valid token",
+                    "error": "authorization_header",
+                }
+            ),
+            401,
+        )
+    
+    
+    @jwt.token_in_blocklist_loader
+    def token_in_blocklist_callback(jwt_header,jwt_data):
+        jti = jwt_data['jti']
+
+        token = db.session.query(TokenBlocklist).filter(TokenBlocklist.jti == jti).scalar()
+
+        return token is not None
+    
+    return app 
+
+if __name__ == "__main__":
+    create_app().run(port=8002)
---
+++ backend/authentication-service/models.py
@@ -0,0 +1,52 @@
+
+from extensions import db
+from uuid import uuid4
+from werkzeug.security import generate_password_hash,check_password_hash
+from datetime import datetime
+
+
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.String(), primary_key=True, default= lambda: str(uuid4()))
+    name = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=False, unique=True)
+    password = db.Column(db.Text())
+
+    
+    def __repr__(self):
+        return f"<User {self.email}>"
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @classmethod
+    def get_user_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class TokenBlocklist(db.Model):
+    __tablename__ = "token_blocklist"
+    id = db.Column(db.Integer(), primary_key=True)
+    jti = db.Column(db.String(), nullable=True)
+    create_at = db.Column(db.DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"<Token {self.jti}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
